409c3d31f71793da5f194e8156386fdc13a66146
WeichenXu <weichenxu1993@icloud.com>
2016-04-07 17:07:35 -0400

    cost function

--- exercise1/ex1/computeCost.m
+++ exercise1/ex1/computeCost.m
@@ -7,12 +7,17 @@ function J = computeCost(X, y, theta)
 m = length(y); % number of training examples
 
 % You need to return the following variables correctly 
-J = 0;
+
 
 % ====================== YOUR CODE HERE ======================
 % Instructions: Compute the cost of a particular choice of theta
 %               You should set J to the cost.
-
+if (m==0),
+  J = 0;
+else,
+  prediction = X*theta;
+  J = (1/(2*m))*sum((prediction-y).^2);
+end;
 
 
 
