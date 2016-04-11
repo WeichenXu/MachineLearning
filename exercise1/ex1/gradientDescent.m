0becf7b28a479159d4efe80e680c548d89a21a60
WeichenXu <weichenxu1993@icloud.com>
2016-04-11 12:03:48 -0400

    linear regression with multiple vars

--- exercise1/ex1/gradientDescent.m
+++ exercise1/ex1/gradientDescent.m
@@ -1,12 +1,12 @@
 function [theta, J_history] = gradientDescent(X, y, theta, alpha, num_iters)
-%GRADIENTDESCENT Performs gradient descent to learn theta
+%   GRADIENTDESCENT Performs gradient descent to learn theta
 %   theta = GRADIENTDESENT(X, y, theta, alpha, num_iters) updates theta by 
 %   taking num_iters gradient steps with learning rate alpha
 
 % Initialize some useful values
 m = length(y); % number of training examples
 J_history = zeros(num_iters, 1);
-
+theta_next = theta;
 for iter = 1:num_iters
 
     % ====================== YOUR CODE HERE ======================
@@ -16,9 +16,9 @@ for iter = 1:num_iters
     % Hint: While debugging, it can be useful to print out the values
     %       of the cost function (computeCost) and gradient here.
     %
-
-
-
+    theta_next = theta - X'*alpha*(1/m)*(X*theta-y);
+    theta = theta_next;
+       
 
 
 
--- exercise1/ex1/gradientDescentMulti.m
+++ exercise1/ex1/gradientDescentMulti.m
@@ -6,7 +6,7 @@ function [theta, J_history] = gradientDescentMulti(X, y, theta, alpha, num_iters
 % Initialize some useful values
 m = length(y); % number of training examples
 J_history = zeros(num_iters, 1);
-
+theta_next = theta;
 for iter = 1:num_iters
 
     % ====================== YOUR CODE HERE ======================
@@ -16,7 +16,8 @@ for iter = 1:num_iters
     % Hint: While debugging, it can be useful to print out the values
     %       of the cost function (computeCostMulti) and gradient here.
     %
-
+    theta_next = theta - alpha*(1/m)*X'*(X*theta-y);
+    theta = theta_next;
 
 
 
